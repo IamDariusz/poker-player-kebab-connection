82a142fe824a4351ad03385d30d88acc30c29708
dgorka <dariusz.gorka@paessler.com>
2020-02-19 12:21:54 +0100

    change back from baris

--- player.py
+++ player.py
@@ -1,18 +1,13 @@
 import json
 
 class Player:
-    VERSION = "Kebab Mestro 0.12"
+    VERSION = "Kebab Mestro 0.13"
 
     def betRequest(self, game_state):
-        json = json.loads(game_state)
-        
         return 25
 
     def showdown(self, game_state):
         pass
-
-    def do_bet(json):
-        return json["current_buy_in"]
 
 
 
