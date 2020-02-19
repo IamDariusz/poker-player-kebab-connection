c4fe87d6899d69b6457548e7d4c80d285234d96d
Baris Kutlu <baris.kutlu@paessler.com>
2020-02-19 12:18:43 +0100

    do it

--- player.py
+++ player.py
@@ -1,17 +1,19 @@
 import json
 
 class Player:
-    VERSION = "Kebab Mestro 0.11"
+    VERSION = "Kebab Mestro 0.12"
 
     def betRequest(self, game_state):
         json = json.loads(game_state)
         
-        
-        return do_bet(json)
+        return 25
 
     def showdown(self, game_state):
         pass
 
     def do_bet(json):
-        return json(["current_buy_in"])
+        return json["current_buy_in"]
+
+
+
 
