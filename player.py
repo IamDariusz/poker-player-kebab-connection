7b02ca034d4fd0dfcf346e0fa1cb6202fc31c148
Ferhat Aram <Ferhat.Aram@paessler.com>
2020-02-19 15:00:38 +0100

    test

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import json
 
 class Player:
-    VERSION = "Kebab Maestro 0.21"
+    VERSION = "Kebab Maestro 1.0"
 
     def check_how_many_active(self, game_state):
         count = 0
@@ -17,25 +17,60 @@ class Player:
             if player["status"] == "active":
                 return player
 
-    def betRequest(self, game_state):
+    def get_me(self, game_state):
         for player in game_state["players"]:
             if player["name"] == "Kebab Connection":
-                if self.check_how_many_active(game_state) > 1:
-                    if player["stack"] < 200:
-                        if game_state["current_buy_in"] < 50:
-                            return 50
-                        else:
-                            return 0
-                    else:
-                        return player["stack"]/2
-                else:
-                    if player["stack"] < 200:
-                        return (self.get_first_non_kebab_active_player(game_state)["stack"])
-                    else:
-                        return (self.get_first_non_kebab_active_player(game_state)["stack"])/2
-            else:
-                continue
+                return player
 
+    def get_ranking(self, game_state):
+        me = self.get_me(game_state)
+        card1 = me["hole_cards"][0]["rank"]
+        card2 = me["hole_cards"][1]["rank"]
+
+        listshit = ["2", "3", "4", "5", "6", "7", "8"]
+
+        ranking = 0
+        # get lost for everything under 9
+        if card1 in listshit or card2 in listshit:
+            if card1 == card2 and card1 > 6:
+                ranking = 0.5
+
+        if card1 == card2 and card1 == "9":
+            ranking = 0.6
+
+        if card1 == card2 and card1 == "T":
+            ranking = 0.66
+
+        list1 = ["A", "Q", "K", "J"]
+        if card1 in list1 and card2 in list1:
+            ranking = 1
+
+        return ranking
+
+    def betRequest(self, game_state):
+        me = self.get_me(game_state)
+        active_players = self.check_how_many_active(game_state)
+        my_ranking = self.get_ranking(game_state)
+
+        if my_ranking == 0:
+            return 0
+
+        if my_ranking == 1:
+            return me["stack"]
+
+        if active_players > 1:
+            if me["stack"] < 200:
+                if game_state["current_buy_in"] < 50:
+                    return 50
+                else:
+                    return 0
+            else:
+                return me["stack"] / 2
+        else:
+            if me["stack"] < 200:
+                return (self.get_first_non_kebab_active_player(game_state)["stack"])
+            else:
+                return (self.get_first_non_kebab_active_player(game_state)["stack"]) / 2
 
     def showdown(self, game_state):
         pass
