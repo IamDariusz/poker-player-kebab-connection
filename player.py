6a68a900c3fb08e5262b0cf21cb818742723b62b
Baris Kutlu <baris.kutlu@paessler.com>
2020-02-19 12:13:50 +0100

    current

--- player.py
+++ player.py
@@ -1,14 +1,17 @@
 import json
 
 class Player:
-    VERSION = "Kebab Mestro 0.1"
+    VERSION = "Kebab Mestro 0.11"
 
     def betRequest(self, game_state):
         json = json.loads(game_state)
         
-
-        return json(["current_buy_in"])
+        
+        return do_bet(json)
 
     def showdown(self, game_state):
         pass
+
+    def do_bet(json):
+        return json(["current_buy_in"])
 
