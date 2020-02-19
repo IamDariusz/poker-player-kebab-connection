5bc8f567fcfa42d5eacd16c3a4074787b553b685
Baris Kutlu <baris.kutlu@paessler.com>
2020-02-19 13:52:21 +0100

    test

--- player.py
+++ player.py
@@ -1,10 +1,14 @@
 import json
 
 class Player:
-    VERSION = "Kebab Mestro 0.16"
+    VERSION = "Kebab Mestro 0.17"
 
     def betRequest(self, game_state):
-        return 996
+        for player in game_state["players"]:
+            if player["name"] == "Kebab Connection":
+                return player["stack"]/2
+            else:
+                continue
 
     def showdown(self, game_state):
         pass
