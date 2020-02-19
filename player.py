6d6ba2a4c704bdabad02c0839d678ec7665b7d61
Ferhat Aram <Ferhat.Aram@paessler.com>
2020-02-19 14:09:55 +0100

    test!

--- player.py
+++ player.py
@@ -3,10 +3,27 @@ import json
 class Player:
     VERSION = "Kebab Maestro 0.18"
 
+    def check_how_many_active(self, game_state):
+        count = 0
+        for player in game_state["players"]:
+            if player["status"] == "active":
+                count = count + 1
+        return count
+
+    def get_first_non_kebab_active_player(self, game_state):
+        for player in game_state["players"]:
+            if player["name"] == "Kebab Connection":
+                continue
+            if player["status"] == "active":
+                return player
+
     def betRequest(self, game_state):
         for player in game_state["players"]:
             if player["name"] == "Kebab Connection":
-                return player["stack"]/2
+                if self.check_how_many_active(game_state) > 1:
+                    return player["stack"]/2
+                else:
+                    return (self.get_first_non_kebab_active_player(game_state)["stack"])/2
             else:
                 continue
 
